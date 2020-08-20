e33dc16d5d75b73fb1d1937bee2f30f974b2b6b7
Abdul Majeed Ayub Inamdar <39568871+Majeed002@users.noreply.github.com>
2020-08-20 11:36:17 +0530

    Navbar website
    Applied background and color to the links in the navbar

--- Assets/Modules/Scripts/app.js
+++ Assets/Modules/Scripts/app.js
@@ -1,3 +1,5 @@
+
+//Navbar Navigation Slider
 const navSlide = () =>{
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
@@ -25,4 +27,6 @@ const navSlide = () =>{
     })
     
 }
+
+
 navSlide();
