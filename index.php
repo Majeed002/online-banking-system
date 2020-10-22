6d5ea05064760e0b4c4451857ae634c9dd1f82f8
Abdul Majeed Ayub Inamdar <39568871+Majeed002@users.noreply.github.com>
2020-10-22 11:07:53 +0530

    footer updated

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="./Assets/Modules/Styles/icons.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./Assets/Modules/Styles/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <script defer src="./Assets/Modules/Scripts/app.js"></script>
     <link rel="icon" href="./Assets/Images/Bank_Logo/Title_icon.png" type="png">
     <title>Urban Bank | Official Website</title>
@@ -35,8 +36,6 @@
             <li>
                 <a href="#">Blogs</a>
             </li>
-            <li>
-                <a href="#">News</a></li>
             <li>
                 <a href="#">Downloads</a>
             </li>
@@ -74,7 +73,7 @@
         <div class="interest-rate-loan-wrapper">
 
             <div class="loan" id="personal-loan-card">
-                <div class="loan-title">
+                <div class="loan-title" id="loan-title">
                     <i class="fas fa-wallet" style="color: rgb(122, 92, 63);"></i>
                     Personal Loan
                 </div>
@@ -95,7 +94,7 @@
             </div>
 
             <div class="loan" id="home-loan-card">
-                <div class="loan-title">
+                <div class="loan-title" id="loan-title">
                     <i class="fas fa-home" style="color: rgb(48, 122, 60);"></i>
                     Home Loan
                 </div>
@@ -112,8 +111,8 @@
                 </div>
             </div>
 
-            <div class="loan" id="Car-loan-card">
-                <div class="loan-title">
+            <div class="loan" id="car-loan-card">
+                <div class="loan-title" id="loan-title">
                     <i class="fas fa-car" style="color: rgba(184, 41, 5, 0.938);"></i>
                     Car Loan
                 </div>
@@ -131,7 +130,7 @@
             </div>
 
             <div class="loan" id="business-loan-card">
-                <div class="loan-title">
+                <div class="loan-title" id="loan-title">
                     <i class="fas fa-chart-line" style="color: rgb(9, 162, 209);"></i>
                     Business Loan
                 </div>
@@ -149,7 +148,7 @@
             </div>
 
             <div class="loan" id="gold-loan-card">
-                <div class="loan-title">
+                <div class="loan-title" id="loan-title">
                     <i class="fas fa-coins" style="color: rgb(204, 185, 98);"></i>
                     Gold Loan
                 </div>
@@ -167,7 +166,7 @@
             </div>
 
             <div class="loan" id="education-loan-card">
-                <div class="loan-title">
+                <div class="loan-title" id="loan-title">
                     <i class="fas fa-graduation-cap" style="color: rgb(75, 75, 75);"></i>
                     Education Loan
                 </div>
@@ -208,11 +207,21 @@
  </div>
 
  <footer id="main-footer">
-        <div class="contact-follow-container">
+    <div class="contact-follow-container">
             
             <div class="contact-us-container">
                 <div class="contact-us-title">
                     CONTACT US
+                </div>
+                <div class="contact-us-details">
+                    <span class="phone-no">
+                    <i class="fas fa-phone-alt"></i>
+                        1800 1234 1324 (Tollfree) / (022) – 42929192 (chargeable number) 24 X 7
+                    </span>
+                    <span class="contact-us-mail">
+                    <i class="fas fa-envelope"></i>
+                    urban.help@gmail.com
+                    </span>
                 </div>
             </div>
             
@@ -220,9 +229,81 @@
                 <div class="follow-us-title">
                     FOLLOW US
                 </div>
+                <div class="social-icons">
+                <i class="fab fa-twitter fa-lg" title="Twitter"></i>
+                <i class="fab fa-facebook-f fa-lg" title="Facebook"></i>
+                <i class="fab fa-linkedin-in fa-lg" title="LinkedIn"></i>
+                <i class="fab fa-instagram fa-lg" title="Instagram"></i>
+                </div>
             </div>
-        </div> 
-    </footer>
+    </div>
+    <div class="footer-body-container">
+        <div class="quick-links-container">
+            <ul>
+                <li class="quick-links-title">Home</li>
+                <li class="quick-links-content">Gallery</li>
+                <li class="quick-links-content">Interest Rate</li>
+                <li class="quick-links-content">Handpicked Offer</li>
+            </ul>
+            <ul>
+                <li class="quick-links-title">Services</li>
+                <li class="quick-links-content">We Provide</li>
+                <li class="quick-links-content">Core Values</li>
+                <li class="quick-links-content">Our Partners</li>
+            </ul>
+            <ul>
+                <li class="quick-links-title">About Us</li>
+                <li class="quick-links-content">History</li>
+                <li class="quick-links-content">Our Mission</li>
+                <li class="quick-links-content">Board of Directors</li>
+                <li class="quick-links-content">Carrers</li>
+            </ul>
+            <ul>
+                <li class="quick-links-title">Blogs</li>
+                <li class="quick-links-content">Investment</li>
+                <li class="quick-links-content">Loans</li>
+                <li class="quick-links-content">Mobile Banking</li>
+                <li class="quick-links-content">Miscellaneous</li>
+            </ul>
+            <ul>
+                <li class="quick-links-title">Legal</li>
+                <li class="quick-links-content">Privacy Policy</li>
+                <li class="quick-links-content">Terms & Conditions</li>
+                <li class="quick-links-content">Disclaimers</li>
+                <li class="quick-links-content">Code of Commitment</li>
+            </ul>
+        </div>
+
+        <div class="get-app-container">
+            Download Mobile App
+            <img src="./Assets/Images/Website-Footer/app-store-badge.png" alt="">
+            <img src="./Assets/Images/Website-Footer/play-store-badge.png" alt="">
+        </div>
+        <div class="newsletter-container">
+            Get in Touch with us
+            <form action="/" method="get">
+            <div class="subscribe-form">
+                    <label for="subscribe">
+                        <i class="far fa-envelope fa-lg"></i>
+                    </label>
+                    <input type="email" id="subscribe" name="subscribe" placeholder="Enter Your Email id..." required>
+            
+            </div>
+            <div class="subscribe-button"> 
+            <button type="submit">
+                Subscribe Now!
+            </button>
+            </div>
+            </form>
+            <span>
+                Subcribe to our newsletter to get daily updates.
+            </span>
+        </div>    
+    </div> 
+    <div class="footer-bottom-container">
+            &copy; 2020 URBAN BANK | All Rights Reserved.
+    </div>
+</footer>
      
 </body>
 </html>
--- services.php
+++ services.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="./Assets/Modules/Styles/icons.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./Assets/Modules/Styles/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <script defer  src="./Assets/Modules/Scripts/app.js"></script>
     <link rel="icon" href="./Assets/Images/Bank_Logo/Title_icon.png" type="png">
     <title>Urban Bank | Official Website</title>
@@ -35,8 +36,6 @@
                 <a href="#">Blogs</a>
             </li>
             <li>
-                <a href="#">News</a></li>
-            <li>
                 <a href="#">Downloads</a>
             </li>
             
@@ -63,11 +62,21 @@
  </div>
 
 <footer id="main-footer">
-        <div class="contact-follow-container">
+    <div class="contact-follow-container">
             
             <div class="contact-us-container">
                 <div class="contact-us-title">
                     CONTACT US
+                </div>
+                <div class="contact-us-details">
+                    <span class="phone-no">
+                    <i class="fas fa-phone-alt"></i>
+                        1800 1234 1324 (Tollfree) / (022) – 42929192 (chargeable number) 24 X 7
+                    </span>
+                    <span class="contact-us-mail">
+                    <i class="fas fa-envelope"></i>
+                    urban.help@gmail.com
+                    </span>
                 </div>
             </div>
             
@@ -75,9 +84,81 @@
                 <div class="follow-us-title">
                     FOLLOW US
                 </div>
+                <div class="social-icons">
+                <i class="fab fa-twitter fa-lg" title="Twitter"></i>
+                <i class="fab fa-facebook-f fa-lg" title="Facebook"></i>
+                <i class="fab fa-linkedin-in fa-lg" title="LinkedIn"></i>
+                <i class="fab fa-instagram fa-lg" title="Instagram"></i>
+                </div>
             </div>
-        </div> 
-    </footer>
+    </div>
+    <div class="footer-body-container">
+        <div class="quick-links-container">
+            <ul>
+                <li class="quick-links-title">Home</li>
+                <li class="quick-links-content">Gallery</li>
+                <li class="quick-links-content">Interest Rate</li>
+                <li class="quick-links-content">Handpicked Offer</li>
+            </ul>
+            <ul>
+                <li class="quick-links-title">Services</li>
+                <li class="quick-links-content">We Provide</li>
+                <li class="quick-links-content">Core Values</li>
+                <li class="quick-links-content">Our Partners</li>
+            </ul>
+            <ul>
+                <li class="quick-links-title">About Us</li>
+                <li class="quick-links-content">History</li>
+                <li class="quick-links-content">Our Mission</li>
+                <li class="quick-links-content">Board of Directors</li>
+                <li class="quick-links-content">Carrers</li>
+            </ul>
+            <ul>
+                <li class="quick-links-title">Blogs</li>
+                <li class="quick-links-content">Investment</li>
+                <li class="quick-links-content">Loans</li>
+                <li class="quick-links-content">Mobile Banking</li>
+                <li class="quick-links-content">Miscellaneous</li>
+            </ul>
+            <ul>
+                <li class="quick-links-title">Legal</li>
+                <li class="quick-links-content">Privacy Policy</li>
+                <li class="quick-links-content">Terms & Conditions</li>
+                <li class="quick-links-content">Disclaimers</li>
+                <li class="quick-links-content">Code of Commitment</li>
+            </ul>
+        </div>
+
+        <div class="get-app-container">
+            Download Mobile App
+            <img src="./Assets/Images/Website-Footer/app-store-badge.png" alt="">
+            <img src="./Assets/Images/Website-Footer/play-store-badge.png" alt="">
+        </div>
+        <div class="newsletter-container">
+            Get in Touch with us
+            <form action="/" method="get">
+            <div class="subscribe-form">
+                    <label for="subscribe">
+                        <i class="far fa-envelope fa-lg"></i>
+                    </label>
+                    <input type="email" id="subscribe" name="subscribe" placeholder="Enter Your Email id..." required>
+            
+            </div>
+            <div class="subscribe-button"> 
+            <button type="submit">
+                Subscribe Now!
+            </button>
+            </div>
+            </form>
+            <span>
+                Subcribe to our newsletter to get daily updates.
+            </span>
+        </div>    
+    </div> 
+    <div class="footer-bottom-container">
+            &copy; 2020 URBAN BANK | All Rights Reserved.
+    </div>
+</footer>
 
 </body>
 </html>
