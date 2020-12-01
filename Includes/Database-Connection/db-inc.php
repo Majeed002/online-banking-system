07e51cddc3c152de0f1bb3a645987ab050a9cca0
Abdul Majeed <inamdarmajeed94@gmail.com>
2020-12-02 00:19:43 +0530

    Database Connection added

---
+++ Includes/Database-Connection/db-inc.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbServerName ="localhost";
+$dbUserName = "root";
+$dbPassword = "";
+$dbName = "urban_bank";
+
+$conn = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
+?>
